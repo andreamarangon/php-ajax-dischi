e498e995b217df2be78cd3c3a3db7b05c31cbc19
Andrea Marangon <marangonandrea84@gmail.com>
2021-05-03 18:09:56 +0200

    milestone 1 terminata

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include './_partials/data.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,16 +12,42 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
   <link rel="stylesheet" href="./dist/css/master.css">
-  <title>Prova</title>
+  <title>PHP DIschi</title>
 </head>
 <body>
   <header>
-    <h1>co</h1>
-
+    <h1 class="title">PHP DIschi</h1>
   </header>
+  <main>
+    <div class="container">
+
+    <?php
+    foreach ($database as $key) {
+    ?>
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div
+           class="flip-card-front"
+           style="background-image: url('<?php echo $key['poster'] ?>')">
+          </div>
+          <div class="flip-card-back">
+            <h2><?php echo $key['title'] ?></h2>
+            <p><?php echo $key['author'] ?></p>
+            <p><?php echo $key['year'] ?></p>
+          </div>
+        </div>
+      </div>
+
+
+    <?php
+    }
+    ?>
+   </div>
+
+  </main>
 
 
 
-  <script src="./dist/js/main.js" charset="utf-8"></script>
+  <!-- <script src="./dist/js/main.js" charset="utf-8"></script> -->
 </body>
 </html>
