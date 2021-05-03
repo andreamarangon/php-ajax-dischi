3d7a45e61e596191b1c1a7e31a6524ce8365624f
Andrea Marangon <marangonandrea84@gmail.com>
2021-05-03 15:50:26 +0200

    primo push

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+  <link rel="stylesheet" href="./dist/css/master.css">
+  <title>Prova</title>
+</head>
+<body>
+  <header>
+    <h1>co</h1>
+
+  </header>
+
+
+
+  <script src="./dist/js/main.js" charset="utf-8"></script>
+</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+mix.sass('src/scss/master.scss', 'dist/css/').options({
+    processCssUrls: false
+});
+
+mix.js('src/js/main.js', 'dist/js/');
+
+
+// mix.setPubblicPath('dist');
